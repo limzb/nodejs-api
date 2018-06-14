3053d5db2e36305b010696e10ce8d1d8e6db3625
Lim Zhe Bin <zhebin9@gmail.com>
2018-06-14 22:06:15 +0800

    Initial commit
    Initialized api repo, setup expressjs environment with webpack hot reload and es6 features

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+const StartServerPlugin = require('start-server-webpack-plugin')
+
+module.exports = {
+    target: 'node',
+    mode: 'development',
+    entry: [
+        'webpack/hot/poll?1000',
+        './src/index'
+    ],
+    watch: true,
+    output: {
+        path: path.resolve(__dirname, '.build'),
+        filename: 'server.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    externals: [nodeExternals({
+        whitelist: ['webpack/hot/poll?1000']
+    })],
+    plugins: [
+        new StartServerPlugin('server.js'),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            "process.env": {
+                "BUILD_TARGET": JSON.stringify('server')
+            }
+        }),
+    ]
+}
